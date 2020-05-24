430cbed8d01ed46355ec8d6bd7c677af95e1cd44
Angel Kozlev <angelkozlev@gmail.com>
2020-05-24 20:57:36 +0100

    Context

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useContext} from "react";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
-    return (
-        <div className="inc-exp-container">
-            <div>
-                <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
-            </div>
-            <div>
-                <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
-            </div>
-        </div>
-    )
-}
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">+$0.00</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">-$0.00</p>
+      </div>
+    </div>
+  );
+};
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,7 +5,7 @@ import AppReducer from "./AppReducer";
 const initialState = {
   transactions: [
     { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 320 },
+    { id: 2, text: "Salary", amount: 330 },
     { id: 3, text: "Book", amount: -10 },
     { id: 4, text: "Camera", amount: 120 },
   ],
