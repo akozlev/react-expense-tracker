d0aa7fb512bdd44efe43e747f3db8bdcc8ce83ca
Angel Kozlev <angelkozlev@gmail.com>
2020-05-25 18:05:41 +0100

    Updates

---
+++ precache-manifest.dc5d5b8d13cee6c8c77713200311c4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68989f0abefb734d59055379b7b11b34",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "02a06f754d594f57a313",
+    "url": "/react-expense-tracker/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a06f754d594f57a313",
+    "url": "/react-expense-tracker/static/js/main.d8c95024.chunk.js"
+  },
+  {
+    "revision": "4752dbf25a34323b5b7c",
+    "url": "/react-expense-tracker/static/js/runtime-main.38b51ddc.js"
+  }
+]);
